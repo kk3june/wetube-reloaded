4222ff162d107c2922ab6a7920c41255a737ebb8
김세준 <kimsejun@3jun-MacBook-Pro.local>
2021-11-17 18:08:34 +0900

    Add: Router, Controller set up

--- src/server.js
+++ src/server.js
@@ -1,5 +1,13 @@
-import express from 'express';
+import express from "express";
+import globalRouter from "./routers/globalRouter";
+import storyRouter from "./routers/storyRouter";
+import userRouter from "./routers/userRouter";
 
 const app = express();
+const PORT = 3000;
 
-console.log('Hello');
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/stories", storyRouter);
+
+app.listen(PORT, () => console.log(`Listening!`));

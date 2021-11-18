3226d7938a55f8a67f3b894d9ea617142827c4d0
3jun <kk3june@gmail.com>
2021-11-19 00:16:58 +0900

    Mod: Video Controller, modify watch views as clicked (~lecture 6.1)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,33 +1,37 @@
+let videos = [
+  {
+    title: "Frist Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 mins ago",
+    views: 52,
+    id: 0,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 mins ago",
+    views: 1,
+    id: 1,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 mins ago",
+    views: 52,
+    id: 2,
+  },
+];
 export const trending = (req, res) => {
-  const videos = [
-    {
-      title: "Frist Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 mins ago",
-      views: 52,
-      id: 0,
-    },
-    {
-      title: "Second Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 mins ago",
-      views: 1,
-      id: 1,
-    },
-    {
-      title: "Third Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 mins ago",
-      views: 52,
-      id: 2,
-    },
-  ];
   return res.render("home", { pageTitle: "Home", videos });
 };
-export const see = (req, res) => res.render("see");
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id];
+  return res.render("watch", { pageTitle: `Watch ${video.title}`, video });
+};
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");

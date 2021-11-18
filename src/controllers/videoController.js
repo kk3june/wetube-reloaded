e733dc29d7f140e5b2381c61a0cbe6e4aacbe2ca
김세준 <kimsejun@3jun-MacBook-Pro.local>
2021-11-18 21:53:25 +0900

    Add: practice Iteration, add  Mixins (chapter5 complete)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,32 @@
-const fakeUser = {
-  username: "3Jun",
-  loggedIn: false,
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "Frist Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 mins ago",
+      views: 52,
+      id: 0,
+    },
+    {
+      title: "Second Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 mins ago",
+      views: 1,
+      id: 1,
+    },
+    {
+      title: "Third Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 mins ago",
+      views: 52,
+      id: 2,
+    },
+  ];
+  return res.render("home", { pageTitle: "Home", videos });
 };
-
-export const trending = (req, res) =>
-  res.render("home", { pageTitle: "Home", fakeUser });
 export const see = (req, res) => res.render("see");
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");

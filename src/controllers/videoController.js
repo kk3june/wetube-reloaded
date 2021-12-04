ccf32340950a2109041b68e080b8f90204565d62
3jun <kk3june@gmail.com>
2021-12-04 17:55:57 +0900

    Mod: add delete fvideo function (lecture 6.25)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -45,6 +45,12 @@ export const postEdit = async (req, res) => {
   return res.redirect(`/videos/${id}`);
 };
 
+export const deleteVideo = async (req, res) => {
+  const { id } = req.params;
+  await Video.findByIdAndDelete(id);
+  return res.redirect("/");
+};
+
 export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };

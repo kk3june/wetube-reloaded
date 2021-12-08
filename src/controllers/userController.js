f62763f3bd3fa62784e7e958cf49132fb4ab0fd7
3jun <kk3june@gmail.com>
2021-12-08 12:12:20 +0900

    Add: validation when singing up(user info, password) (lecture 7.3)

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,7 +2,42 @@ import User from "../models/User";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postJoin = async (req, res) => {
-  const { name, username, email, password, location } = req.body;
+  const { name, username, email, password, password2, location } = req.body;
+  const pageTitle = "Join";
+
+  // const usernameExists = await User.exists({ username });
+  // if (usernameExists) {
+  //   return res.render("Join", {
+  //     pageTitle,
+  //     errorMessage: "This username is already taken.",
+  //   });
+  // }
+
+  // const emailExists = await User.exists({ email });
+  // if (emailExists) {
+  //   return res.render("Join", {
+  //     pageTitle,
+  //     errorMessage: "This email is already taken.",
+  //   });
+  // }
+
+  // usermane, email 동시에 validation
+  const exists = await User.exists({ $or: [{ username }, { email }] });
+  if (exists) {
+    return res.render("Join", {
+      pageTitle,
+      errorMessage: "This username/email is already taken.",
+    });
+  }
+
+  // password validation
+  if (password !== password2) {
+    return res.render("Join", {
+      pageTitle,
+      errorMessage: "Password confirmation does not match.",
+    });
+  }
+
   await User.create({
     name,
     username,

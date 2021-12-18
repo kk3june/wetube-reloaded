24f133c4dfde3f446739ab9b5d1d8977372b2ee4
3jun <kk3june@gmail.com>
2021-12-18 13:47:11 +0900

    Mod: add more user infos using scope and refactoring url(lecture 7.17)

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -79,6 +79,18 @@ export const postLogin = async (req, res) => {
   res.redirect("/");
 };
 
+export const startGithubLogin = (req, res) => {
+  const baseUrl = "https://github.com/login/oauth/authorize";
+  const config = {
+    client_id: "28997268dde17736e014",
+    scope: "read:user user:email",
+  };
+  // URLSearchParams utility
+  const params = new URLSearchParams(config).toString();
+  const finalUrl = `${baseUrl}?${params}`;
+  return res.redirect(finalUrl);
+};
+
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
 export const logout = (req, res) => res.send("Log out");
